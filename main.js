cef9f94e4446c26a4012ec95ca7acf38b00a5675
maya12071989 <puko1207@gmail.com>
2021-06-29 09:18:01 +0900

    first-commit

---
+++ main.js
@@ -0,0 +1,7 @@
+const target = document.getElementById("menu");
+target.addEventListener('click', () => {
+  const target = document.getElementById("menu");
+  target.classList.toggle('open');
+  const nav = document.getElementById("nav2");
+  nav2.classList.toggle('in');
+});
